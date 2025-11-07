98146041eb5fae610826e105d97145174f6276d4
Xyj <983379839@qq.com>
2025-11-07 10:26:15 +0800

    1

--- pro_6/vue.config.js
+++ pro_6/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 使构建后的资源使用相对路径，这样放在 Web-dist/pro6_dist 子目录也能正常加载
+  publicPath: './'
 })
